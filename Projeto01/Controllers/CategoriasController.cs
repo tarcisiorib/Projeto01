56a2c75dd2d157c01f612d19cfe67af7bef3f527
tarcisiorib <tarcisiorib@outlook.com>
2020-11-22 23:30:41 -0300

    Initial

--- Projeto01/Controllers/CategoriasController.cs
+++ Projeto01/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto01.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,66 @@ namespace Projeto01.Controllers
 {
     public class CategoriasController : Controller
     {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria() {CategoriaId = 1, Nome = "Notebooks" },
+            new Categoria() {CategoriaId = 1, Nome = "Monitores" },
+            new Categoria() {CategoriaId = 1, Nome = "Impressoras" },
+            new Categoria() {CategoriaId = 1, Nome = "Mouses" },
+            new Categoria() {CategoriaId = 1, Nome = "Desktops" }
+        };
+
         // GET: Categorias
         public ActionResult Index()
         {
+            return View(categorias);
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(c => c.CategoriaId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(c => c.CategoriaId == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(c => c.CategoriaId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            return RedirectToAction("Index");
         }
     }
 }
